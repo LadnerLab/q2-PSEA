36dd0f3476f7302c7c82012b69dab2ac8f275f87
Oddant1 <oddant1@hotmail.com>
2026-04-27 12:16:38 -0700

    Validating Category probably a waste of time

--- q2_PSEA/actions/epitope.py
+++ q2_PSEA/actions/epitope.py
@@ -24,25 +24,25 @@ def create_epitope_map(
             'EpitopeID').agg(list).reset_index()
     epitope.set_index('EpitopeID', inplace=True)
 
-    def validate_categories(row):
-        '''
-        Ensure the categories column is actually valid. After aggregating some
-        rows will have a list of multiple categories, these should just be the
-        same category multiple times.
+    # def validate_categories(row):
+    #     '''
+    #     Ensure the categories column is actually valid. After aggregating some
+    #     rows will have a list of multiple categories, these should just be the
+    #     same category multiple times.
 
-        1. Ensure that is the case
-        2. Make it just a single value not a list
-        '''
-        category_set = set(row['Category'])
-        if len(category_set) > 1:
-            raise ValueError(
-                'Collapsed epitope mapped some subtypes to one category and '
-                f'some to another. Offending row is: {row}'
-            )
+    #     1. Ensure that is the case
+    #     2. Make it just a single value not a list
+    #     '''
+    #     category_set = set(row['Category'])
+    #     if len(category_set) > 1:
+    #         raise ValueError(
+    #             'Collapsed epitope mapped some subtypes to one category and '
+    #             f'some to another. Offending row is: {row}'
+    #         )
 
-        return row['Category'][0]
+    #     return row['Category'][0]
 
-    epitope['Category'] = epitope.apply(validate_categories, axis=1)
+    # epitope['Category'] = epitope.apply(validate_categories, axis=1)
 
     return epitope
 
