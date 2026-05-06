b0c569508c5fde5fe1113ce56da5efdf3466cde1
eelko09 <55642194+eelko09@users.noreply.github.com>
2026-05-06 14:45:50 -0700

    Fixed naming of peptide_sets
    Fixed the naming of peptide_sets so it now takes in the R lists version

--- q2_PSEA/actions/psea.py
+++ q2_PSEA/actions/psea.py
@@ -384,7 +384,7 @@ def create_fgsea_table_for_pair(
     table = INTERNAL.psea(
         maxZ,
         deltaZ,
-        peptide_sets,
+        peptide_sets_r,
         species_taxa_file,
         threshold,
         permutation_num,
