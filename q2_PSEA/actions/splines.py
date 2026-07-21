a261304e9ccffa8fa491a81bdaa3b845d343bc83
eelko09 <55642194+eelko09@users.noreply.github.com>
2026-07-21 10:45:56 -0700

    Update natural cubic spline
    Updated natural cubic spline to fix the way the spline was displayed in the output scatterplot

--- q2_PSEA/actions/splines.py
+++ q2_PSEA/actions/splines.py
@@ -33,8 +33,8 @@ def smooth_spline(x, y, knots=3, s=0.788458):
     return interpolate.BSpline(t, c, k)(x)
 
 
-def natural_cubic_spline(x, y):
-    """Returns predicted values from a natural cubic spline."""
+def natural_cubic_spline(x, y, knots=3):
+    """Returns predicted values from a natural cubic regression spline."""
     x = np.asarray(x)
     y = np.asarray(y)
 
@@ -46,8 +46,38 @@ def natural_cubic_spline(x, y):
         )
 
     y_unique = np.bincount(inverse, weights=y) / np.bincount(inverse)
-    spline = interpolate.CubicSpline(x_unique, y_unique, bc_type="natural")
-    return spline(x)
+        knot_count = min(knots + 2, len(x_unique))
+    knot_probs = np.linspace(0, 1, knot_count)
+    spline_knots = np.unique(np.quantile(x_unique, knot_probs))
+
+    if len(spline_knots) < 3:
+        return linear_regression(x_unique, y_unique, x_pred=x)
+
+    lower = spline_knots[0]
+    upper = spline_knots[-1]
+
+    def _d(value, knot):
+        return (
+            np.maximum(value - knot, 0) ** 3
+            - np.maximum(value - upper, 0) ** 3
+        ) / (upper - knot)
+
+    basis = [np.ones_like(x_unique), x_unique]
+    reference_knot = spline_knots[-2]
+    reference = _d(x_unique, reference_knot)
+    for knot in spline_knots[:-2]:
+        basis.append(_d(x_unique, knot) - reference)
+
+    design = np.column_stack(basis)
+    coefficients, *_ = np.linalg.lstsq(design, y_unique, rcond=None)
+
+    pred_basis = [np.ones_like(x), x]
+    pred_reference = _d(x, reference_knot)
+    for knot in spline_knots[:-2]:
+        pred_basis.append(_d(x, knot) - pred_reference)
+
+    pred_design = np.column_stack(pred_basis)
+    return pred_design @ coefficients
 
 
 def linear_regression(x, y, x_pred=None, through_origin=False):
