fa46095e8e20650b605d50ece03c2111b952dec2
Oddant1 <oddant1@hotmail.com>
2026-07-29 13:41:41 -0700

    Need to use deltaZ not maxZ

--- q2_PSEA/actions/psea.py
+++ q2_PSEA/actions/psea.py
@@ -647,7 +647,7 @@ def _map_residuals_and_zscores(
         # Get the row associated with the peptide that has the max abs value
         # in maxZ
         max_peptide_row = spline.loc[
-            spline.loc[row['CodeName']]['maxZ'].abs().idxmax()
+            spline.loc[row['CodeName']]['deltaZ'].abs().idxmax()
         ]
         max_peptide_zscores = zscores.loc[max_peptide_row.name]
 
