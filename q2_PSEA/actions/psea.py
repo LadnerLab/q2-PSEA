c96f1788b039dca05b39450a9f6b03a62437d07d
Oddant1 <oddant1@hotmail.com>
2026-07-30 14:09:39 -0700

    Use improved version of _map_residuals_and_zscores

--- q2_PSEA/actions/psea.py
+++ q2_PSEA/actions/psea.py
@@ -781,40 +781,46 @@ def _map_residuals_and_zscores(
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
     zscores = zscores.transpose()
 
-    mapped_spline = pd.DataFrame(columns=spline.columns)
+    spline_name = spline.index.name
     mapped_spline_rows = []
 
-    mapped_zscores = pd.DataFrame(columns=zscores.columns)
     mapped_zscores_rows = []
 
     def map_helper(row):
-        # Get row with peptide with largest residual
-        max_peptide_row = spline.loc[
-            spline.loc[row['CodeName']]['deltaZ'].abs().idxmax()
-        ]
-        max_peptide_zscores = zscores.loc[max_peptide_row.name]
+        # Skip this if this row is a peptide not an epitope. This check shows
+        # that because row['CodeName'] is all peptides, so if row.name matches
+        # the first (and if it's a peptide only) entry in row['CodeName'] the
+        # row must be an uncollapsed peptide
+        if row.name != row['CodeName'][0]:
+            # Get row with peptide with largest residual
+            max_peptide_row = spline.loc[
+                spline.loc[row['CodeName']]['deltaZ'].abs().idxmax()
+            ]
+            max_peptide_zscores = zscores.loc[max_peptide_row.name]
 
-        max_peptide_row.name = row.name
-        max_peptide_zscores.name = row.name
+            max_peptide_row.name = row.name
+            max_peptide_zscores.name = row.name
 
-        mapped_spline_rows.append(max_peptide_row)
-        mapped_zscores_rows.append(max_peptide_zscores)
+            mapped_spline_rows.append(max_peptide_row)
+            mapped_zscores_rows.append(max_peptide_zscores)
+        else:
+            mapped_zscores_rows.append(zscores.loc[row.name])
 
     epitope_map.apply(map_helper, axis=1)
 
-    mapped_spline = pd.concat(
-        [mapped_spline, pd.DataFrame(mapped_spline_rows)],
+    # Add our mapped spline info onto the old ones because we need the
+    # per-peptide spline info to stay around
+    spline = pd.concat(
+        [spline, pd.DataFrame(mapped_spline_rows)],
         ignore_index=False
     )
-    mapped_spline.index.name = spline.index.name
+    spline.index.name = spline_name
 
-    mapped_zscores = pd.concat(
-        [mapped_zscores, pd.DataFrame(mapped_zscores_rows)],
-        ignore_index=False
-    )
+    # We do not need the per peptide z scores anymore, so we overwrite them
+    mapped_zscores = pd.DataFrame(mapped_zscores_rows, columns=zscores.columns)
     mapped_zscores.index.name = zscores.index.name
 
-    return mapped_spline, mapped_zscores
+    return spline, mapped_zscores
 
 
 def _filter_scores_to_pairs(
